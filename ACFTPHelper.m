20ee33cac4b86e862f4b1e1f10cd346cdd7a6db2
Jason Kichline <jkichline@gmail.com>
2011-03-24 23:54:19 -0400

    Initial release

---
+++ ACFTPHelper.m
@@ -0,0 +1,40 @@
+//
+//  FTPHelper.m
+//  OnSong
+//
+//  Created by Jason Kichline on 3/24/11.
+//  Copyright 2011 andCulture. All rights reserved.
+//
+
+#import "ACFTPHelper.h"
+
+@implementation ACFTPHelper
+
++(NSURL*)urlByRemovingCredentials:(NSURL*)input {
+	NSString* str = [input absoluteString];
+	NSRange from = [str rangeOfString:@"://"];
+	NSRange to = [str rangeOfString:@"@"];
+	str = [NSString stringWithFormat:@"%@%@", [str substringToIndex:from.location + from.length], [str substringFromIndex:to.location + to.length]];
+	return [NSURL URLWithString:str];
+}
+
++(NSURL*)urlByAddingCredentials:(NSURL*)input username:(NSString*)username password:(NSString*)password {
+	NSString* str = [input absoluteString];
+	
+	// If we already have credentials, send it out
+	if(username == nil || [str rangeOfString:@"@"].length > 0) { 
+		return input;
+	}
+	
+	// Insert the username/password
+	NSMutableString* o = [NSMutableString stringWithString:str];
+	NSRange at = [str rangeOfString:@"://"];
+	NSString* pwd = @"";
+	if(password != nil) {
+		pwd = [NSString stringWithFormat:@":%@", password];
+	}
+	[o insertString:[NSString stringWithFormat:@"%@%@@", username, pwd] atIndex:at.location + at.length];
+	return [NSURL URLWithString:o];
+}
+
+@end
